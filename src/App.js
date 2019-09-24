f3b8831c98087b73432f12228f1d3910d5329e0b
abby-lammers <abigaillammers2019@u.northwestern.edu>
2019-09-24 00:37:09 -0500

    display unstyled product list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState} from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({}); // initialize data as an empty array with {}
+  const products = Object.values(data);//Object.values(data); // creates an object, products, which holds data (products.json)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(Object.values(json)[0]);
+    };
+    fetchProducts();
+  }, []); // If the second arg is an empty list [], then the useEffect() function will only be called when the component is added (i.e., once).
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {products.map(product => <li key={product.sku}>{product.title}</li>)} 
+    </ul>
   );
-}
+};
 
 export default App;
